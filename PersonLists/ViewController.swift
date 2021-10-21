5d0ffa48ee4d050177249a75bb3d072c0dc5017c
Alexandr <zhenuhin@inbox.ru>
2021-10-21 15:14:07 +0300

    Initial Commit

---
+++ PersonLists/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonLists
+//
+//  Created by Александр Женухин on 21.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
